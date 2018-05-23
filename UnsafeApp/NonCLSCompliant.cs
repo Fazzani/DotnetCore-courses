90afa070eb927e7279208e89cfdbb8709a0e0574
Heni FAZZANI <heni.fazzani@seloger.com>
2018-05-23 11:19:36 +0200

    Ajoutez des fichiers projet.

---
+++ UnsafeApp/NonCLSCompliant.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+[assembly: CLSCompliant(true)]
+namespace NonCLS
+{
+    public class NonCLSCompliant
+    {
+        /// <summary>
+        /// two or more public / protected / protected internal members defined with only case difference
+        /// </summary>
+        public int intA = 0;
+        public int INTA = 2;
+
+        /// <summary>
+        /// two or more public / protected / protected internal members defined with only case difference
+        /// </summary>
+        public int x = 0;
+        public void X()
+        {
+        }
+
+        /// <summary>
+        /// Declaring a member name preceding underscore (_) 
+        /// with outside visible access specifiers (public, protected, protected internal)
+        /// </summary>
+        public void _test()
+        {
+
+        }
+
+        /// <summary>
+        /// Declaring unsigned type member with access public / protected / protected internal
+        /// </summary>
+        public uint a = 10;
+
+        /// <summary>
+        /// Declaring unsafe (pointer) type with access public / protected / protected internal
+        /// </summary>
+        protected internal unsafe int* c;
+
+        /// <summary>
+        /// A (public / protected / protected internal) method returning a non-CLS compliant type 
+        /// or with parameter of non-CLS compliant type
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public uint testmethod(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void anothermethod(ushort x, int y)
+        {
+        }
+
+
+
+    }
+
+    /// <summary>
+    /// An abstract member marked as non-CLS-Compliant in a CLS-Compliant class
+    /// </summary>
+    [CLSCompliant(true)]
+    public abstract class B
+    {
+        [CLSCompliant(false)]
+        protected internal abstract void test();
+    }
+}
---
+++ UnsafeApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace UnsafeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pointers.Example1();
+            Pointers.Example2();
+            ReadKey();
+            
+        }
+
+        unsafe static void FastCopy(byte[] src, byte[] dst, int count)
+        {
+            Span<int> block = stackalloc int[3] { 1, 2, 3 };
+        }
+    }
+}
